cb7212daeb608fb92acb5896e4be93e197812645
Vishvajeet Prasad <vishvajeetp@pvh.com>
2025-09-27 22:37:05 +0530

    changed data file

--- src/test/java/com/utility/PropertiesUtil.java
+++ src/test/java/com/utility/PropertiesUtil.java
@@ -10,7 +10,7 @@ import com.constants.Env;
 
 public class PropertiesUtil {
 	public static String readProperty(Env env, String propertyName){
-		File propFile = new File(System.getProperty("user.dir")+"\\Config\\" + env +".properties");
+		File propFile = new File(System.getProperty("user.dir")+"/Config/" + env +".properties");
 		FileReader fileReader;
 		Properties properties =new Properties();
 		try {
